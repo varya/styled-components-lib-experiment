af788433f6f9941debb85c5df7c959978b185998
Varya Stepanova <mail@varya.me>
2017-11-02 21:18:10 +0200

    Demonstrate using external library in styleguidist

--- app/components/Button/A.js
+++ app/components/Button/A.js
@@ -2,6 +2,10 @@ import styled from 'styled-components';
 
 import buttonStyles from './buttonStyles';
 
-const A = styled.a`${buttonStyles}`;
+import patternLibrary from '../../../libs/external-pattern-library.css';
+
+const A = styled.a.attrs({
+  className: 'pl-button'
+})`${buttonStyles}`;
 
 export default A;
