39df83ef143bbfaac8ac3cfe19792a4f71489435
Varya Stepanova <mail@varya.me>
2017-11-02 20:38:39 +0200

    Writing stories in the component folders

---
+++ app/components/A/A.stories.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import { Button, Welcome } from '@storybook/react/demo';
+
+import A from './index.js';
+
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
+storiesOf('Button', module)
+  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+
+storiesOf('A', module)
+  .add('styled-component link', () => <A href="http://sc5.io">Click here</A>)
